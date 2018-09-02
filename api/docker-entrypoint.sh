2d2abf952130ce493f9a3920e3b8d96888331f0a
Flannery <flannj@gmail.com>
2018-09-02 15:15:52 -0400

    intial commit - docker setup for django and mysql db

---
+++ api/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Install python package dependencies"
+cd /app
+pwd
+ls
+
+echo "Install python package dependencies"
+pipenv install --system
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
---
+++ database/db-setup.sql
@@ -0,0 +1,3 @@
+CREATE USER 'flannerykj'@'%' IDENTIFIED BY 'cheesecake';
+GRANT ALL PRIVILEGES ON *.* TO 'flannerykj'@'%' WITH GRANT OPTION;
+CREATE DATABASE shakespeare_db;
